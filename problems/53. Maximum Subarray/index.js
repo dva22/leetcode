9e7dc187b8a8cda0d5260a242c6aba0f0ef8e746
dva22 <dva@farma.amur.ru>
2018-04-20 14:22:43 +0900

    53. Maximum Subarray
    add 53. Maximum Subarray

---
+++ problems/53. Maximum Subarray/index.js
@@ -0,0 +1,31 @@
+// Source : https://leetcode.com/problems/add-two-numbers/description/
+// Author : Dubchak Valery
+
+/***
+Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+Follow up:
+
+If you have figured out the O(n) solution, try coding another solution using the divide and conquer approach, which is more subtle.
+
+***/
+
+
+var maxSubArray = module.exports = function(nums) {
+  var maxn = -Number.MAX_VALUE;
+  var sum = 0;
+  nums.forEach(function(item, index, array) {
+    sum += item;
+    if (sum > maxn)
+      maxn = sum;
+    if (sum < 0)
+      sum = 0;
+  });
+
+  return maxn;
+};
---
+++ problems/53. Maximum Subarray/testcases.js
@@ -0,0 +1,8 @@
+var ListNode = require('../../structures/ListNode');
+
+module.exports = [
+  {
+    input: [[-2,1,-3,4,-1,2,1,-5,4]],
+    output: 6// ListNode.generateList(807),
+  }
+];
