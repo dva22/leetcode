84bc4cac0b21d0fda73b1239d612ae9ce6cf1984
dva22 <dva@farma.amur.ru>
2018-07-30 08:55:37 +0900

    add 877. Stone Game

---
+++ problems/877. Stone Game/index.js
@@ -0,0 +1,24 @@
+// Source : https://leetcode.com/problems/stone-game/description/
+
+// Author : Dubchak Valery (dva@farma.amur.ru)
+
+/***
+Alex and Lee play a game with piles of stones.  There are an even number of piles arranged in a row, and each pile has a positive integer number of stones piles[i].
+
+The objective of the game is to end with the most stones.  The total number of stones is odd, so there are no ties.
+
+Alex and Lee take turns, with Alex starting first.  Each turn, a player takes the entire pile of stones from either the beginning or the end of the row.  This continues until there are no more piles left, at which point the person with the most stones wins.
+
+Assuming Alex and Lee play optimally, return True if and only if Alex wins the game.
+
+ 
+
+Example 1:
+
+Input: [5,3,4,5]
+Output: true
+***/
+ 
+var stoneGame = module.exports = function(piles) {
+    return true
+};
---
+++ problems/877. Stone Game/testcases.js
@@ -0,0 +1,9 @@
+var ListNode = require('../../structures/ListNode');
+var buildTree = require('../../structures/TreeNode');
+
+module.exports = [
+  {
+    input: [[5,3,4,5]],
+    output: true
+  }
+];
