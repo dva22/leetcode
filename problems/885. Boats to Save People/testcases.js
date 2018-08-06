35f44c7d7862b1541ef2114d94f896050155c51c
dva22 <dva@farma.amur.ru>
2018-08-06 10:05:27 +0900

    add 885. Boats to Save People

---
+++ problems/885. Boats to Save People/testcases.js
@@ -0,0 +1,17 @@
+var ListNode = require('../../structures/ListNode');
+var buildTree = require('../../structures/TreeNode');
+
+module.exports = [
+  {
+    input: [[1,2], 3],
+    output: 1
+  },
+  {
+    input: [[3,2,2,1], 3],
+    output: 3
+  },
+  {
+    input: [[3,5,3,4], 5],
+    output: 4
+  },
+];
