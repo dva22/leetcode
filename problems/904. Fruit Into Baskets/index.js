5f5110959df57e4d2f34d755cc162c8113349b6d
dva22 <dva@farma.amur.ru>
2018-09-18 15:09:15 +0900

    add 904. Fruit Into Baskets

---
+++ problems/904. Fruit Into Baskets/index.js
@@ -0,0 +1,35 @@
+// Source : https://leetcode.com/problems/fruit-into-baskets/description/
+// Author : Dubchak Valery
+
+/***
+In a row of trees, the i-th tree produces fruit with type tree[i].
+
+You start at any tree of your choice, then repeatedly perform the following steps:
+
+Add one piece of fruit from this tree to your baskets.  If you cannot, stop.
+Move to the next tree to the right of the current tree.  If there is no tree to the right, stop.
+Note that you do not have any choice after the initial choice of starting tree: you must perform step 1, then step 2, then back to step 1, then step 2, and so on until you stop.
+
+You have two baskets, and each basket can carry any quantity of fruit, but you want each basket to only carry one type of fruit each.
+
+What is the total amount of fruit you can collect with this procedure?
+
+ 
+
+Example 1:
+
+Input: [1,2,1]
+Output: 3
+
+***/
+
+var totalFruit = module.exports = function(tree) {
+    let res = 0, cur = 0, count_b = 0, a = 0, b = 0;
+        for (c of  tree) {
+            cur = c == a || c == b ? cur + 1 : count_b + 1;
+            count_b = c == b ? count_b + 1 : 1;
+            if (b != c) {a = b; b = c;}
+            res = Math.max(res, cur);
+        }
+        return res;
+};
---
+++ problems/904. Fruit Into Baskets/testcases.js
@@ -0,0 +1,20 @@
+var ListNode = require('../../structures/ListNode');
+
+module.exports = [
+ {
+    input: [[1,2,1]],
+    output: 3
+  },
+  {
+    input: [[0,1,2,2]],
+    output: 3
+  },
+  {
+    input: [[1,2,3,2,2]],
+    output: 4
+  },
+  {
+    input: [[3,3,3,1,2,1,1,2,3,3,4]],
+    output: 5
+  }
+];
