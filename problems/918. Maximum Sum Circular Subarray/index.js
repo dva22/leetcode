f75db273b303336e2f5fdf8e414e52a76d1e97e0
dva22 <dva@farma.amur.ru>
2018-10-08 16:14:32 +0900

    add 918. Maximum Sum Circular Subarray

---
+++ problems/918. Maximum Sum Circular Subarray/index.js
@@ -0,0 +1,29 @@
+// Source : https://leetcode.com/problems/maximum-sum-circular-subarray/description/
+// Author : Dubchak Valery
+
+/***
+Given a circular array C of integers represented by A, find the maximum possible sum of a non-empty subarray of C.
+
+Here, a circular array means the end of the array connects to the beginning of the array.  (Formally, C[i] = A[i] when 0 <= i < A.length, and C[i+A.length] = C[i] when i >= 0.)
+
+Also, a subarray may only include each element of the fixed buffer A at most once.  (Formally, for a subarray C[i], C[i+1], ..., C[j], there does not exist i <= k1, k2 <= j with k1 % A.length = k2 % A.length.)
+
+ 
+
+Example 1:
+
+Input: [1,-2,3,-2]
+Output: 3
+
+***/
+var maxSubarraySumCircular = module.exports = function(A) {
+        let total = 0, maxSum = -30000, curMax = 0, minSum = 30000, curMin = 0;
+        for (a of A) {
+            curMax = Math.max(curMax + a, a);
+            maxSum = Math.max(maxSum, curMax);
+            curMin = Math.min(curMin + a, a);
+            minSum = Math.min(minSum, curMin);
+            total += a;
+        }
+        return maxSum > 0 ? Math.max(maxSum, total - minSum) : maxSum;
+};
---
+++ problems/918. Maximum Sum Circular Subarray/testcases.js
@@ -0,0 +1,16 @@
+var ListNode = require('../../structures/ListNode');
+
+module.exports = [
+ {
+    input: [[1,-2,3,-2]],
+    output: 3
+  },
+  {
+    input: [[5,-3,5]],
+    output: 10
+  },
+  {
+    input: [[3,-2,2,-3]],
+    output: 3
+  }
+];
