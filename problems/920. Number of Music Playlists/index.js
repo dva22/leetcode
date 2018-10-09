d58c97ef2f9bf0bc02a775097b9da995daf895e3
dva22 <dva@farma.amur.ru>
2018-10-09 20:56:12 +0900

    add 920. Number of Music Playlists

---
+++ problems/920. Number of Music Playlists/index.js
@@ -0,0 +1,35 @@
+// Source : https://leetcode.com/problems/number-of-music-playlists/description/
+// Author : Dubchak Valery
+
+/***
+Your music player contains N different songs and she wants to listen to L (not necessarily different) songs during your trip.  You create a playlist so that:
+
+Every song is played at least once
+A song can only be played again only if K other songs have been played
+Return the number of possible playlists.  As the answer can be very large, return it modulo 10^9 + 7.
+
+ 
+
+Example 1:
+
+Input: N = 3, L = 3, K = 1
+Output: 6
+
+***/
+
+var numMusicPlaylists = module.exports = function(N, L, K) {
+     const MAGIC = 10 ** 9 + 7
+    let f = new Array(L + 1).fill(0).map(a => new Array(N + 1).fill(0))
+    f[0][0] = 1
+    // f[i][j] represents the number of playlists that its length is i and it contains j different songs
+    for(let i = 1; i <= L; i++) {
+        for (let j = 1; j <= Math.min(i, N); j++) {
+            // Add a new song to playlist
+            f[i][j] += f[i - 1][j - 1] * (N - j + 1)
+            // Add an existed song to playlist
+            f[i][j] += f[i - 1][j] * Math.max(j - K, 0)
+            f[i][j] = f[i][j] % MAGIC
+        }
+    }
+    return f[L][N]
+};
---
+++ problems/920. Number of Music Playlists/testcases.js
@@ -0,0 +1,16 @@
+var ListNode = require('../../structures/ListNode');
+
+module.exports = [
+ {
+    input: [3, 3, 1],
+    output: 6
+  },
+  {
+    input: [2, 3,  0],
+    output: 6
+  },
+  {
+    input: [2,  3,  1],
+    output: 2
+  }
+];
