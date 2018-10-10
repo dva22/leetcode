e1200b06616d7d1580ccf23bc25120f8f26d877e
dva22 <dva@farma.amur.ru>
2018-10-10 14:58:55 +0900

    Update problems/920. Number of Music Playlists/index.js

--- problems/920. Number of Music Playlists/index.js
+++ problems/920. Number of Music Playlists/index.js
@@ -21,6 +21,7 @@ var numMusicPlaylists = module.exports = function(N, L, K) {
      const MAGIC = 10 ** 9 + 7
     let f = new Array(L + 1).fill(0).map(a => new Array(N + 1).fill(0))
     f[0][0] = 1
+    
     // f[i][j] represents the number of playlists that its length is i and it contains j different songs
     for(let i = 1; i <= L; i++) {
         for (let j = 1; j <= Math.min(i, N); j++) {
