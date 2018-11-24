1fcfad7be1cc2d28dbd89800bd140c993d974107
dva22 <dva@farma.amur.ru>
2018-11-24 13:44:15 +0900

    add 613. Shortest distance in a line

---
+++ problems/613. Shortest distance in a line/index.js
@@ -0,0 +1,27 @@
+// Source : https://leetcode.com/problems/shortest-distance-in-a-plane
+// Author : Dubchak Valery
+
+/***
+Table point holds the x coordinate of some points on x-axis in a plane, which are all integers.
+Write a query to find the shortest distance between two points in these points.
+| x   |
+|-----|
+| -1  |
+| 0   |
+| 2   |
+The shortest distance is '1' obviously, which is from point '-1' to '0'. So the output is as below:
+| shortest|
+|---------|
+| 1       |
+Note: Every point is unique, which means there is no duplicates in table point.
+Follow-up: What if all these points have an id and are arranged from the left most to the right most of x axis?
+
+***/
+
+/* Write your T-SQL query statement below */
+/***
+SELECT MIN(ABS(p1.x - p2.x)) AS shortest
+FROM point p1
+JOIN point p2
+ON p1.x <> p2.x
+*///
